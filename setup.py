ff575e9824b52f6762eb4be8b02d95d575ce0395
Yasser Souri <yassersouri@gmail.com>
2019-07-11 18:43:32 +0200

    exclude some packages from the setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/yassersouri/fandak",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['docs', 'examples', 'tests', 'scripts']),
     install_requires=[
         "torch>=1.0,<2",
         "torchvision>=0.3,<1",
