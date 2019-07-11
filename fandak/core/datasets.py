0b5ddfeb44b78541a903c679b8e7a7bf8bef9be7
Yasser Souri <yassersouri@gmail.com>
2019-07-11 19:57:58 +0200

    add dataset's default batch type
    it supports default collate

--- fandak/core/datasets.py
+++ fandak/core/datasets.py
@@ -1,25 +1,47 @@
-from json import dumps as jdumps
-from typing import List, Tuple
+from dataclasses import dataclass
+from typing import List
 
+from torch import Tensor
 from torch.utils.data import Dataset as tDataset
+# noinspection PyUnresolvedReferences
+from torch.utils.data.dataloader import default_collate
+from yacs.config import CfgNode
+
+from fandak.utils.torch import GeneralDataClass
+
+
+@dataclass
+class GeneralBatchType(GeneralDataClass):
+    @classmethod
+    def default_collate(cls, items: List["GeneralBatchType"]) -> "GeneralBatchType":
+        """
+        Uses default_collate from `torch.utils.data.Dataloader.default_collate`
+        for every attribute of the type to collate them together.
+        """
+        values = {}
+        for attr_name in items[0].get_attribute_names():
+            if isinstance(getattr(items[0], attr_name), Tensor):
+                attrs = [getattr(i, attr_name) for i in items]
+                values[attr_name] = default_collate(attrs)
+
+        # noinspection PyArgumentList
+        return cls(**values)
 
 
 class Dataset(tDataset):
+    def __init__(self, cfg: CfgNode):
+        super().__init__()
+        self.cfg = cfg
+
     def __len__(self) -> int:
         raise NotImplementedError
 
-    def __getitem__(self, item: int):
+    def __getitem__(self, item: int) -> GeneralBatchType:
         raise NotImplementedError
 
     @staticmethod
-    def collate_fn(items: List[Tuple]) -> Tuple:
+    def collate_fn(items: List[GeneralBatchType]) -> GeneralBatchType:
         """
         Each dataset class should implement its own collate function.
         """
         raise NotImplementedError
-
-    def json_repr(self) -> dict:
-        raise NotImplementedError
-
-    def __repr__(self) -> str:
-        return jdumps(self.json_repr(), sort_keys=True)
--- fandak/utils/torch.py
+++ fandak/utils/torch.py
@@ -1,9 +1,10 @@
 import random
-from typing import Union, Collection
+from typing import Union, Collection, List
 
 import numpy as np
 import torch
 from torch import Tensor
+from torch.utils.data import Sampler
 
 from fandak.utils.misc import is_listy
 
@@ -48,3 +49,48 @@ def tensor_to_numpy(
         return tuple(map(lambda t: tensor_to_numpy(t), x))
     else:
         return x.detach().cpu().numpy()
+
+
+class OverfitSampler(Sampler):
+    def __init__(self, main_source, indices):
+        super().__init__(main_source)
+        self.main_source = main_source
+        self.indices = indices
+
+        main_source_len = len(self.main_source)
+
+        how_many = int(round(main_source_len / len(self.indices)))
+        self.to_iter_from = []
+        for _ in range(how_many):
+            self.to_iter_from.extend(self.indices)
+
+    def __iter__(self):
+        return iter(self.to_iter_from)
+
+    def __len__(self):
+        return len(self.main_source)
+
+
+class GeneralDataClass:
+    def get_attribute_names(self) -> List[str]:
+        return [
+            a
+            for a in dir(self)
+            if not a.startswith("__") and not callable(getattr(self, a))
+        ]
+
+    def to(self, device: torch.device):
+        for attr_name in self.get_attribute_names():
+            attr = getattr(self, attr_name)
+            if isinstance(attr, torch.Tensor):
+                setattr(self, attr_name, attr.to(device))
+
+    def item(self):
+        """
+        This is like calling `.item()` for all of the attributes and setting their values to it.
+        Will raise error if more than a single item is inside the tensor.
+        """
+        for attr_name in self.get_attribute_names():
+            attr = getattr(self, attr_name)
+            if isinstance(attr, torch.Tensor):
+                setattr(self, attr_name, attr.item())
