e400e8f49c1b473066a6daab8628983add4f2048
Yasser Souri <yassersouri@gmail.com>
2019-07-11 20:50:25 +0200

    refactor fandak models

--- fandak/core/models.py
+++ fandak/core/models.py
@@ -1,43 +1,49 @@
 from abc import ABC
-from json import dumps
-from typing import List, Dict, Tuple
+from dataclasses import dataclass
+from typing import List, Dict
 
 import torch.nn as nn
 from torch import Tensor
+from yacs.config import CfgNode
+
+from fandak.core.datasets import GeneralBatch
+from fandak.utils.torch import GeneralDataClass
+
+
+@dataclass
+class GeneralLoss(GeneralDataClass):
+    """
+    I assume that there is always the `main` attribute which is the loss that is
+    going to be used for backpropagation.
+    """
+
+    main: Tensor
+
+
+@dataclass
+class GeneralForwardOut(GeneralDataClass):
+    """
+    The general output of the forward pass.
+    """
+
+    pass
 
 
 class Model(nn.Module, ABC):
-    def __init__(self, **kwargs):
+    def __init__(self, cfg: CfgNode):
         super().__init__()
+        self.cfg = cfg
 
     def get_params(self, original_lr: float) -> List[Dict]:
         params = [{"params": self.parameters(), "lr": original_lr}]
         return params
 
-    def prepare_forward_input_from_batch(
-        self, batch: Tuple[Tensor, ...], test_mode: bool = False
-    ) -> Dict[str, Tensor]:
+    # noinspection PyMethodMayBeStatic
+    def get_backprop_loss(self, loss: GeneralLoss) -> Tensor:
+        return loss.main
+
+    def forward(self, batch: GeneralBatch) -> GeneralForwardOut:
         raise NotImplementedError
 
-    def prepare_loss_input(
-        self, batch: Tuple[Tensor, ...], forward_out: Tuple[Tensor, ...]
-    ) -> Dict[str, Tensor]:
-        raise NotImplementedError
-
-    def get_backprop_loss(self, losses: Tuple[Tensor, ...]) -> Tensor:
-        if type(losses) == tuple:
-            return losses[0]
-        else:
-            return losses
-
-    def forward(self, **kwargs: Tensor) -> Tuple[Tensor, ...]:
-        raise NotImplementedError
-
-    def loss(self, **kwargs: Tensor) -> Tuple[Tensor, ...]:
-        raise NotImplementedError
-
-    def __repr__(self) -> str:
-        return dumps(self.json_repr(), sort_keys=True, indent=4)
-
-    def json_repr(self) -> dict:
+    def loss(self, batch: GeneralBatch, forward_out: GeneralForwardOut) -> GeneralLoss:
         raise NotImplementedError
