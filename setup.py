43a2e2713682db987528d662322213f0c92f04e9
Yasser Souri <yassersouri@gmail.com>
2019-07-18 11:53:36 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="fandak",
-    version="0.0.3",
+    version="0.0.4",
     author="Yasser Souri",
     author_email="yassersouri@gmail.com",
     description="A Framework for Deep Learning Research in PyTorch.",
