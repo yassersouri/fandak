99894d2ef34e97f12c0d8852dda1c62b542a968e
Yasser Souri <yassersouri@gmail.com>
2019-07-18 14:41:41 +0200

    update classification example to report val loss

--- examples/classification_mnist/proj/evaluators.py
+++ examples/classification_mnist/proj/evaluators.py
@@ -14,31 +14,43 @@ from proj.models import ForwardOut
 @dataclass
 class TrainEvaluationResult(GeneralEvaluatorResult):
     accuracy: float
+    average_loss: float
 
 
 class ValidationEvaluator(Evaluator):
+    def get_name(self) -> str:
+        return "val"
+
     # noinspection PyAttributeOutsideInit
     def set_storage(self):
         self.preds = []
         self.targets = []
+
+        self.loss = []
 
     def on_finish_eval(self) -> TrainEvaluationResult:
         preds = np.array(self.preds)
         targets = np.array(self.targets)
 
         accuracy = sum(preds == targets) / len(preds)
+        average_loss = sum(self.loss) / len(self.loss)
 
         print_with_time("Evaluation accuracy: %1.6f" % accuracy)
+        print_with_time("Evaluation average loss: %1.6f" % average_loss)
 
-        return TrainEvaluationResult(accuracy=accuracy)
+        return TrainEvaluationResult(accuracy=accuracy, average_loss=average_loss)
 
     def batch_eval_calculation(self, batch: Batch, forward_out: ForwardOut):
         preds = tensor_to_numpy(forward_out.logits.argmax(dim=1))
         targets = tensor_to_numpy(batch.y)
 
+        loss = self.model.loss(batch=batch, forward_out=forward_out)
+
         for i in range(len(targets)):
             self.preds.append(preds[i])
             self.targets.append(targets[i])
+
+            self.loss.append(loss.main.item())
 
     def create_dataloader(self) -> DataLoader:
         batch_size = self.cfg.evaluator.eval_batch_size
